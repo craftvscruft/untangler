4040ae34ca01c7f511115ce2ff7a7ef6d45b5756
Ray Myers <rmyers@indeed.com>
2023-08-13 09:52:23 -0500

    Error handling and refactoring

--- src/main/java/ai/mender/Console.java
+++ src/main/java/ai/mender/Console.java
@@ -1,12 +1,24 @@
 package ai.mender;
 
-import ai.mender.domain.FunctionRec;
 import com.fasterxml.jackson.core.JsonProcessingException;
-
-import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class Console {
-    public static <T> void printJson(T result) throws JsonProcessingException {
-        System.out.println(Language.toJson(result));
+    public static <T> void printJson(T result) {
+        try {
+            System.out.println(toJson(result));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> String toJson(T object) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper() {};
+
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        return objectMapper
+                .setDefaultPrettyPrinter(new JsonPrettyPrinter())
+                .writeValueAsString(object);
     }
 }
--- src/main/java/ai/mender/Language.java
+++ src/main/java/ai/mender/Language.java
@@ -1,22 +1,14 @@
 package ai.mender;
 
+import ai.mender.parsing.ThrowingErrorListener;
 import antlrgen.CPP14Lexer;
 import antlrgen.CPP14Parser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.misc.Interval;
 
 public class Language {
-    public static <T> String toJson(T object) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper() {};
-
-        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-        return objectMapper.setDefaultPrettyPrinter(new JsonPrettyPrinter()).writeValueAsString(object);
-    }
 
     public static String getTextIncludingWhitespace(ParserRuleContext ctx) {
         if (null == ctx) {
@@ -27,12 +19,18 @@ public class Language {
         return ctx.start.getInputStream().getText(new Interval(a, b));
     }
 
-    public static CPP14Parser.TranslationUnitContext parseProgram(CharStream inputStream) {
+    public static CPP14Parser.TranslationUnitContext parseProgram(
+            CharStream inputStream, boolean throwOnParseError) {
         CPP14Lexer lexer = new CPP14Lexer(inputStream);
-//        lexer.addErrorListener(ThrowingErrorListener.INSTANCE);
+        if (throwOnParseError) {
+            lexer.addErrorListener(ThrowingErrorListener.INSTANCE);
+        }
+
         CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
         CPP14Parser parser = new CPP14Parser(commonTokenStream);
-//        parser.addErrorListener(ThrowingErrorListener.INSTANCE);
+        if (throwOnParseError) {
+            parser.addErrorListener(ThrowingErrorListener.INSTANCE);
+        }
         return parser.translationUnit();
     }
 }
